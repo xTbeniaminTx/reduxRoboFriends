b656116bd919422faf760f34340768a3788a19fe
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2021-02-11 18:48:52 +0100

    redux middlware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from "redux";
+import {createLogger} from "redux-logger/src";
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import {searchRobots} from "./reducers";
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
